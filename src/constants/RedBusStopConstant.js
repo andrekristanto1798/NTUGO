1d2532302e9f21ed6efdbafa3aad9ac3dcc60f2a
ekoeditaa <ekoeditaa@gmail.com>
2018-07-29 15:25:23 +0700

    add red bus stop details

--- src/constants/RedBusStopConstant.js
+++ src/constants/RedBusStopConstant.js
@@ -1,9 +1,113 @@
 const HALL_12 = {
   position: {
-    latitude: 1.351952,
-    longitude: 103.680521,
+    latitude: 1.351932,
+    longitude: 103.680606,
   },
   name: 'Hall 12 Bus stop',
+};
+
+const LWN = {
+  position: {
+    latitude: 1.347973,
+    longitude: 103.680514,
+  },
+  name: 'Outside Canteen A & Lee Wee Nam Library / Opposite NIE'
+};
+
+const CEE = {
+  position: {
+    latitude: 1.346230,
+    longitude: 103.678526,
+  },
+  name: 'Outside School of Civil & Environmental Engineering (N1)'
+};
+
+const SBS = {
+  position: {
+    latitude: 1.344602,
+    longitude: 103.678669,
+  },
+  name: 'Outside School of Biological Sciences'
+};
+
+const WKW = {
+  position: {
+    latitude: 1.342081,
+    longitude: 103.679230,
+  },
+  name: 'Outside Wee Kim Wee School of Communications & Information (S1)'
+};
+
+const HALL_7 = {
+  position: {
+    latitude: 1.339987,
+    longitude: 103.680895,
+  },
+  name: 'Outside Hall of Residence 7'
+};
+
+const INNOVATION_CENTRE = {
+  position: {
+    latitude: 1.342293,
+    longitude: 103.683725,
+  },
+  name: 'Outside Innovation Centre, Canteen B & Chinese Heritage Centre'
+};
+
+const HALL_4 = {
+  position: {
+    latitude: 1.343840,
+    longitude: 103.686866,
+  },
+  name: 'Outside Hall of Residence 4'
+};
+
+const HALL_1 = {
+  position: {
+    latitude: 1.345696,
+    longitude: 103.687810,
+  },
+  name: 'Outside Hall of Residence 1'
+};
+
+const CANTEEN_2 = {
+  position: {
+    latitude: 1.349064, 
+    longitude: 103.685385,
+  },
+  name: 'Outside Canteen 2'
+};
+
+const HALL_9 = {
+  position: {
+    latitude: 1.351526,
+    longitude: 103.685696,
+  },
+  name: 'Opposite Nanyang Heights / Beside Hall of Residence 9'
+};
+
+const HALL_10 = {
+  position: {
+    latitude: 1.354343,
+    longitude: 103.686858,
+  },
+  name: 'Outside Hall of Residence 10 & 11'
+};
+
+const HALL_11 = {
+  position: {
+    latitude: 1.355866,
+    longitude: 103.686110,
+  },
+  name: 'Outside Graduate Hall / Hall of Residence 11'
+};
+
+const TAMARIND_HALL = {
+  position: {
+    latitude: 1.354950,
+    longitude: 103.684439,
+  },
+  name: 'Nanyang Crescent Halls'
 };
 
 const MOCK_RED_BUS_1 = {
@@ -34,4 +138,4 @@ const MOCK_RED_BUS_3 = {
 };
 
 export const RED_BUS_LIST = [MOCK_RED_BUS_1, MOCK_RED_BUS_2, MOCK_RED_BUS_3];
-export const RED_BUS_STOP_LIST = [HALL_12];
+export const RED_BUS_STOP_LIST = [HALL_12, LWN, CEE, SBS, WKW, HALL_7, INNOVATION_CENTRE, HALL_4, HALL_1, CANTEEN_2, HALL_9, HALL_10, HALL_11, TAMARIND_HALL];
