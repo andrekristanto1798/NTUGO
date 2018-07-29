ca63b20d3eb84e051161442f118f3eb1ede941f3
Andre Kristanto <akristan001@e.ntu.edu.sg>
2018-07-29 16:39:10 +0800

    Add Blue Bus Stop information (Issue #2)

---
+++ src/constants/BlueBusStopConstant.js
@@ -0,0 +1,30 @@
+const OPP_HALL_10_11 = { name: 'Opp. Hall 10 & 11', position: { latitude: 1.354668, longitude: 103.687445 } };
+const OPP_HALL_8 = { name: 'Opp. Hall 8', position: { latitude: 1.350511, longitude: 103.68588 } };
+const OPP_HALL_2 = { name: 'Hall 6, Opp. Hall 2', position: { latitude: 1.347303, longitude: 103.686575 } };
+const OPP_HALL_4 = { name: 'Opp. Hall 4', position: { latitude: 1.343637, longitude: 103.686739 } };
+const OPP_INNOV_CENTRE = { name: 'Opp. Innovation Centre', position: { latitude: 1.341891, longitude: 103.683863 } };
+const OPP_SPMS = { name: 'Opp. SPMS', position: { latitude: 1.341843, longitude: 103.681094 } };
+const OPP_WKWSCI = { name: 'Opp. WKWSCI', position: { latitude: 1.341983, longitude: 103.679056 } };
+const OPP_CEE = { name: 'Opp. CEE', position: { latitude: 1.345862, longitude: 103.678414 } };
+const OPP_LWN = { name: 'NIE, Opp. LWN Library', position: { latitude: 1.347969, longitude: 103.68024 } };
+const OPP_HALL_3 = { name: 'Opp. Hall 3 & 16', position: { latitude: 1.350485, longitude: 103.68038 } };
+const OPP_HALL_14 = { name: 'Opp. Hall 14 & 15', position: { latitude: 1.353155, longitude: 103.681656 } };
+const OPP_HALL_TAMARIN = {
+  name: 'Opp Nanyang Crescent Hall',
+  position: { latitude: 1.354785, longitude: 103.683856 },
+};
+
+export const BLUE_BUS_STOP_LIST = [
+  OPP_HALL_10_11,
+  OPP_HALL_8,
+  OPP_HALL_2,
+  OPP_HALL_4,
+  OPP_INNOV_CENTRE,
+  OPP_SPMS,
+  OPP_WKWSCI,
+  OPP_CEE,
+  OPP_LWN,
+  OPP_HALL_3,
+  OPP_HALL_14,
+  OPP_HALL_TAMARIN,
+];
