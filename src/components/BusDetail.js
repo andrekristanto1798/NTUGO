8885b00a5557019cc335a43232b9443558e42c79
ekoeditaa <ekoeditaa@gmail.com>
2018-07-29 21:15:00 +0700

    add toggle button

--- src/components/BusDetail.js
+++ src/components/BusDetail.js
@@ -1,31 +1,42 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, Dimensions } from 'react-native';
+import { Button } from '../../node_modules/react-native-elements';
 
 export default class BusDetail extends Component {
   render() {
     const { busStopName, busType, nextArrival, subsequentArrival } = this.props;
 
     return (
-      <View style={styles.container}>
-        <View style={styles.row}>
-          <View style={[styles.box]}>
-            <Text style={styles.title}> { busStopName } </Text>
+      <View style={[styles.container]}>
+        <View style={[styles.toggler]}>
+          <Button color="#333"
+            title="toggle details"
+            onPress={this.toggleDetails}>
+          </Button>
+        </View>
+        <View style={[styles.row]}>
+          <View style={[styles.box, styles.header]}>
+            <Text style={styles.title}> {busStopName} </Text>
           </View>
         </View>
-        <View style={styles.row}>
+        <View style={[styles.row]}>
           <View style={[styles.box, styles.box2]}>
-            <Text style={styles.text}> { busType } </Text>
+            <Text style={[styles.text]}> {busType} </Text>
           </View>
           <View style={[styles.box]}>
-            <Text style={styles.text}> { nextArrival } </Text>
+            <Text style={[styles.text]}> {nextArrival} </Text>
           </View>
           <View style={[styles.box, styles.box3]}>
-            <Text style={styles.text}> { subsequentArrival } </Text>
+            <Text style={[styles.text]}> {subsequentArrival} </Text>
           </View>
         </View>
       </View>
     );
   }
+}
+
+toggleDetails = () => {
+  // method to toggle the component rendering
 }
 
 // calculate window size to place 
@@ -41,13 +52,12 @@ const styles = StyleSheet.create({
     height: 120,
     left: 0,
     top: WINDOW_HEIGHT - 120,
-    width: WINDOW_WIDTH,
-    justifyContent: 'center'
+    width: WINDOW_WIDTH
   },
   row: {
     flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     margin: 0
   },
   box: {
@@ -61,16 +71,24 @@ const styles = StyleSheet.create({
   box3: {
     backgroundColor: '#4A79A5'
   },
+  toggler: {
+    height: 10,
+    backgroundColor: '#333',
+    width: '100%'
+  },
   two: {
     flex: 2
   },
   text: {
     textAlign: 'center',
+    color: 'white',
     alignItems: 'center',
+    fontSize: 18
   },
   title: {
     textAlign: 'center',
     fontSize: 20,
-  },
+    color: 'white'
+  }
 });
 
