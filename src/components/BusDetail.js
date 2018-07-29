91d5f70f1d45c0e35a0d7f9dece17ae7e2e1bc02
ekoeditaa <ekoeditaa@gmail.com>
2018-07-29 18:26:54 +0700

    add props to bus detail component

--- src/components/BusDetail.js
+++ src/components/BusDetail.js
@@ -3,22 +3,24 @@ import { Text, View, StyleSheet, Dimensions } from 'react-native';
 
 export default class BusDetail extends Component {
   render() {
+    const { busStopName, busType, nextArrival, subsequentArrival } = this.props;
+
     return (
       <View style={styles.container}>
-        <View style={styles.flex}>
-          <Text style={styles.title}> Bus Stop Detail </Text>
+        <View style={styles.row}>
+          <View style={[styles.box]}>
+            <Text style={styles.title}> { busStopName } </Text>
+          </View>
         </View>
-        <View style={{
-          flexDirection: 'row'
-        }}>
-          <View style={styles.flex}>
-            <Text style={styles.text}> Campus Loop Red </Text>
+        <View style={styles.row}>
+          <View style={[styles.box, styles.box2]}>
+            <Text style={styles.text}> { busType } </Text>
           </View>
-          <View style={styles.flex}>
-            <Text style={styles.text}> 5 mins </Text>
+          <View style={[styles.box]}>
+            <Text style={styles.text}> { nextArrival } </Text>
           </View>
-          <View style={styles.flex}>
-            <Text style={styles.text}> 10 mins </Text>
+          <View style={[styles.box, styles.box3]}>
+            <Text style={styles.text}> { subsequentArrival } </Text>
           </View>
         </View>
       </View>
@@ -26,34 +28,49 @@ export default class BusDetail extends Component {
   }
 }
 
-let {height, width} = Dimensions.get('window');
-
+// calculate window size to place 
+// the component at the bottom
+let { height, width } = Dimensions.get('window');
 const WINDOW_HEIGHT = height;
 const WINDOW_WIDTH = width;
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'column',
+    borderTopWidth: 1,
+    borderTopColor: '#000',
     height: 120,
-    padding: 20,
-    left: 0, 
-    top: WINDOW_HEIGHT - 120, 
+    left: 0,
+    top: WINDOW_HEIGHT - 120,
     width: WINDOW_WIDTH,
+    justifyContent: 'center'
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 0
+  },
+  box: {
+    flex: 1,
+    height: '100%',
+    backgroundColor: '#31659C',
+  },
+  box2: {
+    backgroundColor: '#639ACE'
+  },
+  box3: {
+    backgroundColor: '#4A79A5'
+  },
+  two: {
+    flex: 2
   },
   text: {
-    textAlign: 'center'
+    textAlign: 'center',
+    alignItems: 'center',
   },
   title: {
     textAlign: 'center',
-    fontSize: 20
+    fontSize: 20,
   },
-  contentContainer: {
-    flex: 1 // pushes the footer to the end of the screen
-  },
-  footer: {
-    height: 100
-  },
-  flex: {
-    flex: 1
-  }
 });
+
