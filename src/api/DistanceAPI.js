69ad6f73c1b2ba14b90cb7843415e33f84c044a0
ekoeditaa <ekoeditaa@gmail.com>
2018-07-30 13:30:15 +0700

    add bus arrival estimator

---
+++ src/api/DistanceAPI.js
@@ -0,0 +1,20 @@
+const BASE_URL = 'https://maps.googleapis.com/maps/api/distancematrix';
+const FORMAT_JSON_AND_UNIT = '/json?units=imperial';
+const API_KEY = '&key=AIzaSyAwKBu8x9waeq4bmqh_JB-EVxWn5oIsZrQ';
+const ORIGIN = '&origins='
+const DESTINATION = '&destinations='
+const GET = 'get';
+
+async function getEstimatedTime(busStop, busPosition) {
+    let finalURL = BASE_URL + FORMAT_JSON_AND_UNIT 
+                    + ORIGIN + busPosition.position.latitude + "," + busPosition.position.longitude
+                    + DESTINATION + busStop.position.latitude + "," + busStop.position.longitude
+                    + API_KEY;
+    const request = {
+      method: GET,
+      mode: 'no-cors'
+    };
+    return fetch(finalURL, request)
+      .then(response => response.json())
+      .catch(error => console.log(error));
+}
